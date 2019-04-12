fd315d9a41d295f997d01f472e1d8830184df332
katherine232 <48964831+katherine232@users.noreply.github.com>
2019-04-11 20:51:38 -0400

    ESPYTECH
    04/11/2019 9:00 pm

---
+++ ESPYTECH/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing    bec',
+            'author':'Richard Orfao',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit  Malandro.',
+            'author':'Malandro',
+            'create_date':'99-99-2017'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'Lorem ',
+            'author':'Choro',
+            'create_date':'44-44-4444'
+        }
+    ]
+    
+    return articles
